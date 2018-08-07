ae6ad3f0ae2dce2f8e7ff7d6a3aacd8a18ed2725
PatrickMarlowe <36053211+PatrickMarlowe@users.noreply.github.com>
2018-08-07 17:25:46 -0400

    I added the date scrollers to the enter data sections in the xml and java files

--- PeeDeeHealthAdvisor/app/src/main/java/com/example/pmarl/peedeehealthadvisor/EnterBloodSugarDataActivity.java
+++ PeeDeeHealthAdvisor/app/src/main/java/com/example/pmarl/peedeehealthadvisor/EnterBloodSugarDataActivity.java
@@ -12,17 +12,40 @@
 package com.example.pmarl.peedeehealthadvisor;
 
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class EnterBloodSugarDataActivity extends AppCompatActivity
 {
+
     private ImageButton home;
     private ImageButton backBtn;
+
+    private Context context = this;
+    private EditText editDate;
+    private Calendar myCalendar = Calendar.getInstance();
+    private String dateFormat = "MM.dd.yyyy";
+    private DatePickerDialog.OnDateSetListener date;
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat, Locale.US);
+
+
+
 
      @Override
     protected void  onCreate(Bundle saveInstanceState)
@@ -30,25 +53,62 @@ public class EnterBloodSugarDataActivity extends AppCompatActivity
          super.onCreate(saveInstanceState);
          setContentView(R.layout.acativty_enter_blood_sugar_data);
 
-        home = (ImageButton) findViewById(R.id.Home);
+         editDate = (EditText) findViewById(R.id.editDate);
 
-        home.setOnClickListener(new View.OnClickListener() {
+         //set date to current date
+         long currentdate = System.currentTimeMillis();
+         String dateString = simpleDateFormat.format(currentdate);
+         editDate.setText(dateString);
+
+         //set calendar date and update editDate
+         date = new DatePickerDialog.OnDateSetListener()
+         {
+             @Override
+             public void onDateSet(DatePicker datePicker, int year, int month, int dayOfmonth)
+             {
+                 myCalendar.set(Calendar.YEAR, year);
+                 myCalendar.set(Calendar.MONTH, month);
+                 myCalendar.set(Calendar.DAY_OF_MONTH, dayOfmonth);
+                 updateDate();
+             }
+         };
+
+         //Onclick popup datepicker
+         editDate.setOnClickListener(new View.OnClickListener()
+         {
+             @Override
+             public void onClick(View view)
+             {
+                 new DatePickerDialog(context, date, myCalendar.get(Calendar.YEAR),
+                         myCalendar.get(Calendar.MONTH),
+                         myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+             }
+         });
+
+         home = (ImageButton) findViewById(R.id.Home);
+
+         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 launchMainActivity();
             }
         });
 
-        backBtn = (ImageButton) findViewById(R.id.BackBtn);
+         backBtn = (ImageButton) findViewById(R.id.BackBtn);
 
-        backBtn.setOnClickListener(new View.OnClickListener() {
+         backBtn.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View view) {
                  launchprevActivity();
              }
          });
 
+     }
 
+
+     private void updateDate()
+     {
+         editDate.setText(simpleDateFormat.format(myCalendar.getTime()));
      }
 
 
--- PeeDeeHealthAdvisor/app/src/main/java/com/example/pmarl/peedeehealthadvisor/EnterCholesterolDataActivity.java
+++ PeeDeeHealthAdvisor/app/src/main/java/com/example/pmarl/peedeehealthadvisor/EnterCholesterolDataActivity.java
@@ -12,16 +12,32 @@
  */
 package com.example.pmarl.peedeehealthadvisor;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class EnterCholesterolDataActivity extends AppCompatActivity
 {
     private ImageButton home;
     private ImageButton backBtn;
+
+    private Context context = this;
+    private EditText editDate;
+    private Calendar myCalendar = Calendar.getInstance();
+    private String dateFormat = "MM.dd.yyyy";
+    private DatePickerDialog.OnDateSetListener date;
+    private SimpleDateFormat sdf = new SimpleDateFormat(dateFormat, Locale.US);
+
 
 
     /*************************************************************************************
@@ -33,6 +49,44 @@ public class EnterCholesterolDataActivity extends AppCompatActivity
     {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_enter_cholesterol_data);
+
+        editDate = (EditText) findViewById(R.id.editDate);
+
+        // init - set date to current date
+        long currentdate = System.currentTimeMillis();
+        String dateString = sdf.format(currentdate);
+        editDate.setText(dateString);
+
+        // set calendar date and update editDate
+        date = new DatePickerDialog.OnDateSetListener()
+        {
+
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth)
+            {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateDate();
+            }
+
+        };
+
+        // onclick - popup datepicker
+        editDate.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v)
+            {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(context, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
 
         home = (ImageButton) findViewById(R.id.Home);
@@ -54,7 +108,10 @@ public class EnterCholesterolDataActivity extends AppCompatActivity
         });
     }
 
-
+    private void updateDate()
+    {
+        editDate.setText(sdf.format(myCalendar.getTime()));
+    }
 
     private void launchMainActivity()
     {
