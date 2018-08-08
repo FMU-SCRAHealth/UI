6c85e50baf57f0a5032cbdf63a1914405e78be20
PatrickMarlowe <36053211+PatrickMarlowe@users.noreply.github.com>
2018-08-07 20:14:37 -0400

    I added the date scrollers to the enter data sections in the xml and java files (#3)

--- PeeDeeHealthAdvisor/app/src/main/java/com/example/pmarl/peedeehealthadvisor/EnterBloodPressureDataActivity.java
+++ PeeDeeHealthAdvisor/app/src/main/java/com/example/pmarl/peedeehealthadvisor/EnterBloodPressureDataActivity.java
@@ -11,22 +11,75 @@
  */
 package com.example.pmarl.peedeehealthadvisor;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class EnterBloodPressureDataActivity extends AppCompatActivity //github test
 {
     private ImageButton home;
     private ImageButton backBtn;
 
+    Context context = this;
+    EditText editDate;
+    Calendar myCalendar = Calendar.getInstance();
+    String dateFormat = "MM.dd.yyyy";
+    DatePickerDialog.OnDateSetListener date;
+    SimpleDateFormat sdf = new SimpleDateFormat(dateFormat, Locale.US);
+
     @Override
     protected void  onCreate(Bundle saveInstanceState)
     {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_enter_blood_pressure_data);
+
+        editDate = (EditText) findViewById(R.id.editDate);
+
+// init - set date to current date
+        long currentdate = System.currentTimeMillis();
+        String dateString = sdf.format(currentdate);
+        editDate.setText(dateString);
+
+// set calendar date and update editDate
+        date = new DatePickerDialog.OnDateSetListener()
+        {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth)
+            {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateDate();
+            }
+
+        };
+
+// onclick - popup datepicker
+        editDate.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v)
+            {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(context, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
         home = (ImageButton) findViewById(R.id.Home);
 
@@ -47,6 +100,11 @@ public class EnterBloodPressureDataActivity extends AppCompatActivity //github t
         });
 
 
+    }
+
+    private void updateDate()
+    {
+        editDate.setText(sdf.format(myCalendar.getTime()));
     }
 
     private void launchMainActivity()
