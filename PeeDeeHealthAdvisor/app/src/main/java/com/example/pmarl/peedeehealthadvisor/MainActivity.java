4903ad4d6d11e5737188bc73be5617fb5230b054
Matt Harrington <mharr1729@gmail.com>
2018-10-25 23:05:36 -0400

    new screens added with layout changes

--- PeeDeeHealthAdvisor/app/src/main/java/com/example/pmarl/peedeehealthadvisor/MainActivity.java
+++ PeeDeeHealthAdvisor/app/src/main/java/com/example/pmarl/peedeehealthadvisor/MainActivity.java
@@ -93,7 +93,7 @@ public class MainActivity extends AppCompatActivity
 
     private void launchMyHealthData()
     {
-        Intent intent = new Intent (this, MyHealthDataActivity.class);
+        Intent intent = new Intent (this, SelectDataActivity.class);
         intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK | intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
         finish();
---
+++ PeeDeeHealthAdvisor/app/src/main/java/com/example/pmarl/peedeehealthadvisor/SelectCholesterolActivity.java
@@ -0,0 +1,102 @@
+/*
+    Author: Matt Harrington
+    Email Address: matt2harrington@gmail.com
+    Written: October 25th, 2018
+    Last Updated: October 25th, 2018
+
+    Compilation:
+    Execution:
+
+    Description of Execution:
+ */
+package com.example.pmarl.peedeehealthadvisor;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class SelectCholesterolActivity extends AppCompatActivity
+{
+    private ImageButton home;
+    private Button graph;
+    private Button enterData;
+
+
+    @Override
+    protected void  onCreate(Bundle saveInstanceState)
+    {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.select_cholesterol_activity);
+
+        this.home = (ImageButton) findViewById(R.id.Home);
+
+        this.home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchMainActivity();
+            }
+        });
+
+        this.graph = (Button) findViewById(R.id.ViewData);
+
+        this.graph.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchGraphActivity();
+            }
+        });
+
+        this.enterData = (Button) findViewById(R.id.EnterData);
+
+        this.enterData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchSelectDataActivity();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        launchPrevActivity();
+    }
+
+
+    private void launchMainActivity()
+    {
+        Intent intent = new Intent (this, MainActivity.class);
+        intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK | intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+
+    }
+
+    private void launchPrevActivity()
+    {
+        Intent intent = new Intent (this, SelectDataActivity.class);
+        intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK | intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+
+    }
+
+    private void launchGraphActivity()
+    {
+        Intent intent = new Intent(this, CholesterolGraph.class);
+        intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK | intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+    private void launchSelectDataActivity()
+    {
+        Intent intent = new Intent(this, EnterCholesterolDataActivity.class);
+        intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK | intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+}
