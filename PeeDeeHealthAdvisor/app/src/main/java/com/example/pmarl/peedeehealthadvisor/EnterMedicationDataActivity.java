8d2f0b8992fc09eec3a56cdf14f91c84bbe1570b
fmu <fmu@fmus-MacBook-Pro.local>
2019-01-27 16:16:48 -0500

    update

---
+++ PeeDeeHealthAdvisor/app/src/main/java/com/example/pmarl/peedeehealthadvisor/EnterMedicationDataActivity.java
@@ -0,0 +1,170 @@
+package com.example.pmarl.peedeehealthadvisor;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class EnterMedicationDataActivity extends AppCompatActivity
+{
+
+    //DatabaseHelper myDB;
+    private Button enterData;
+    private Button clearData;
+    private TextInputEditText medNameInput, medDoseInput, medDelivery, medRxNum, medPharmName, medPhoneNum;
+    private Context context = this;
+
+    @Override
+    protected void  onCreate(Bundle saveInstanceState)
+    {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_enter_med_data);
+
+        ImageButton home = findViewById(R.id.Home);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchMainActivity();
+            }
+        });
+
+        medNameInput = (TextInputEditText) findViewById(R.id.medicationNameInput);
+        medDoseInput = (TextInputEditText) findViewById(R.id.medDoseInput);
+        medDelivery = (TextInputEditText) findViewById(R.id.medDeliveryInput);
+        medRxNum = (TextInputEditText) findViewById(R.id.medRXInput);
+        medPharmName = (TextInputEditText) findViewById(R.id.medPharmNameInput);
+        medPhoneNum = (TextInputEditText) findViewById(R.id.medPharmNumberInput);
+
+
+        enterData = (Button) findViewById(R.id.enterData);
+
+        enterData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final String userFullName = medNameInput.getText().toString() + " " +
+                        medNameInput.getText().toString();
+                if (medNameInput.getText().toString().equals("") || medDoseInput.getText().toString().equals("")) {
+                    showDataNotEnteredWarning();
+
+                } else {
+
+                    boolean isInserted =
+                            MainActivity.myDB.insertAllergies(medNameInput.getText().toString(),
+                                    medDoseInput.getText().toString());
+
+                    if (isInserted = true)
+                        showDataEntryCheckmark();
+                    else
+                        showDataError();
+
+                    launchPrevActivity();
+                }
+            }
+        });
+
+        clearData = (Button) findViewById(R.id.clearData);
+
+        clearData.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                medNameInput.setText("");
+                medDoseInput.setText("");
+                medDelivery.setText("");
+                medRxNum.setText("");
+                medPharmName.setText("");
+                medPhoneNum.setText("");
+
+            }
+        });
+
+
+        LinearLayout mainLayout = (LinearLayout)findViewById(R.id.medicationEntry);
+        mainLayout.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // TODO Auto-generated method stub
+                InputMethodManager inputMethodManager = (InputMethodManager)  EnterMedicationDataActivity.this.getSystemService(Activity.INPUT_METHOD_SERVICE);
+                inputMethodManager.hideSoftInputFromWindow(EnterMedicationDataActivity.this.getCurrentFocus().getWindowToken(), 0);
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        launchPrevActivity();
+    }
+
+    private void launchMainActivity()
+    {
+        Intent intent = new Intent (this, MainActivity.class);
+        intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK | intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+
+    }
+
+    private void launchPrevActivity()
+    {
+        Intent intent = new Intent (this, SelectMedicationActivity.class);
+        intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK | intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+    // Show images in Toast prompt.
+    private void showDataEntryCheckmark()
+    {
+
+        Toast toast = Toast.makeText(getApplicationContext(), "Allergy Entered", Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        LinearLayout toastContentView = (LinearLayout) toast.getView();
+        ImageView imageView = new ImageView(getApplicationContext());
+        imageView.setImageResource(R.drawable.ic_checkmark);
+        toastContentView.addView(imageView, 0);
+        toast.show();
+
+    }
+
+    // Show images in Toast prompt.
+    private void showDataNotEnteredWarning()
+    {
+        Toast toast = Toast.makeText(getApplicationContext(), "Please Complete All Fields", Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        LinearLayout toastContentView = (LinearLayout) toast.getView();
+        ImageView imageView = new ImageView(getApplicationContext());
+        imageView.setImageResource(R.drawable.ic_warning);
+        toastContentView.addView(imageView, 0);
+        toast.show();
+    }
+
+    // Show images in Toast prompt.
+    private void showDataError()
+    {
+
+        Toast toast = Toast.makeText(getApplicationContext(), "ERROR: Please Try Again", Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        LinearLayout toastContentView = (LinearLayout) toast.getView();
+        ImageView imageView = new ImageView(getApplicationContext());
+        imageView.setImageResource(R.drawable.ic_error);
+        toastContentView.addView(imageView, 0);
+        toast.show();
+    }
+}
---
+++ PeeDeeHealthAdvisor/app/src/main/java/com/example/pmarl/peedeehealthadvisor/MedicationTable.java
@@ -0,0 +1,100 @@
+package com.example.pmarl.peedeehealthadvisor;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+public class MedicationTable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_medications);
+
+        Cursor cursorMedications = MainActivity.myDB.readMedData();
+
+        String medName = "";
+        String medDose = "";
+        String medDelivery = "";
+        String medRXnum = "";
+        String medPharmName = "";
+        String medPharmNum = "";
+        TextView medicationName = findViewById(R.id.medNameText);
+        TextView medicationDose = findViewById(R.id.medDoseText);
+        TextView medicationDelivery = findViewById(R.id.medDeliveryText);
+        TextView medicationRxNum = findViewById(R.id.medRXNumText);
+        TextView medicationPharmName = findViewById(R.id.medPharnNameText);
+        TextView medicationPharmNum = findViewById(R.id.medPharmNumText);
+
+
+        ImageButton home = findViewById(R.id.Home);
+        TableLayout tableLayout = findViewById(R.id.medTable);
+
+        // ALLERGY BOX UPDATE
+
+        // Check for values (ALLERGIES)
+        if (cursorMedications != null) {
+            cursorMedications.moveToFirst();
+        }
+
+        // iterate
+        while (!cursorMedications.isAfterLast()) {
+
+            medName= cursorMedications.getString(0);
+            medDose = cursorMedications.getString(1);
+            medDelivery = cursorMedications.getString(2);
+            medRXnum = cursorMedications.getString(3);
+            medPharmName = cursorMedications.getString(4);
+            medPharmNum = cursorMedications.getString(5);
+
+            TableLayout table = (TableLayout)MedicationTable.this.findViewById(R.id.medTable);
+
+            // Inflate your row "template" and fill out the fields.
+            TableRow row = (TableRow) LayoutInflater.from(MedicationTable.this).inflate(R.layout.med_row, null);
+            ((TextView)row.findViewById(R.id.medNameText)).setText(medName);
+            ((TextView)row.findViewById(R.id.medDoseText)).setText(medDose);
+            table.addView(row);
+
+            table.requestLayout();
+            cursorMedications.moveToNext();
+        }
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchMainActivity();
+            }
+        });
+
+    }
+
+    //Button Functionality
+    @Override
+    public void onBackPressed() {
+        launchPrevActivity();
+    }
+
+    private void launchMainActivity()
+    {
+        Intent intent = new Intent (this, MainActivity.class);
+        intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK | intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+
+    }
+
+    private void launchPrevActivity()
+    {
+        Intent intent = new Intent (this, SelectMedicationActivity.class);
+        intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK | intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+}
