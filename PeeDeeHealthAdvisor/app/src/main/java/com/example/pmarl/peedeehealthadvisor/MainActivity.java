547727e3062e9df08cd29559c6d42fda8645a7c7
Matt Harrington <mattharrington@Matts-MacBook-Pro-2.local>
2019-04-12 12:53:39 -0400

    fixed issue with notifications by checking for name from input

--- PeeDeeHealthAdvisor/app/src/main/java/com/example/pmarl/peedeehealthadvisor/MainActivity.java
+++ PeeDeeHealthAdvisor/app/src/main/java/com/example/pmarl/peedeehealthadvisor/MainActivity.java
@@ -169,11 +169,11 @@ public class MainActivity extends AppCompatActivity
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel channel1 = new NotificationChannel(
                     "CHANNEL_1_ID",
-                    "Blood Pressure Notifications",
+                    "Flu Shot Notifications",
                     NotificationManager.IMPORTANCE_HIGH
             );
 
-            channel1.setDescription("Blood Pressure Notifications");
+            channel1.setDescription("Flu Shot Notifications");
 
 
             NotificationManager manager = getSystemService(NotificationManager.class);
@@ -183,32 +183,32 @@ public class MainActivity extends AppCompatActivity
 
             NotificationChannel channel2 = new NotificationChannel(
                     "CHANNEL_2_ID",
-                    "Blood Sugar Notifications",
+                    "Shingrix (RZV) Notifications",
                     NotificationManager.IMPORTANCE_HIGH
             );
 
-            channel2.setDescription("Blood Sugar Notifications");
+            channel2.setDescription("Shingrix (RZV) Notifications");
 
             manager.createNotificationChannel(channel2);
 
 
             NotificationChannel channel3 = new NotificationChannel(
                     "CHANNEL_3_ID",
-                    "Cholesterol Notifications",
+                    "Pneumonia PPSV23 Notifications",
                     NotificationManager.IMPORTANCE_HIGH
             );
 
-            channel3.setDescription("Cholesterol Notifications");
+            channel3.setDescription("Pneumonia PPSV23 Notifications");
 
             manager.createNotificationChannel(channel3);
 
             NotificationChannel channel4 = new NotificationChannel(
                     "CHANNEL_4_ID",
-                    "Vaccination Notifications",
+                    "Pneumonia PVC13 Notifications",
                     NotificationManager.IMPORTANCE_HIGH
             );
 
-            channel4.setDescription("Vaccinations Notifications");
+            channel4.setDescription("Pneumonia PVC13 Notifications");
 
             manager.createNotificationChannel(channel4);
 
